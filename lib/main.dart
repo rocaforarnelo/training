e277544530c58569e0c25eab05073d378549f1b8
F <f@thinkbitsolutions.com>
2021-09-17 12:09:16 +0800

    Another comment

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,7 @@ class MyHomePage extends StatefulWidget {
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
+  //Final Comment
 
   final String title;
 
